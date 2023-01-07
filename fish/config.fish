50092a91383890a8eccb1fa5d90a0aada069315d
mx_in <mouxin1993@gmail.com>
2023-01-08 01:37:07 +0800

    chore(fish_config):reorder

--- fish/config.fish
+++ fish/config.fish
@@ -1,58 +1,68 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-fish_config theme choose "Rosé Pine"
+#!/bin/fish
 
 # Initialize Tools
-starship init fish | source
-zoxide init fish | source
+function init_tools
+    starship init fish | source
+    zoxide init fish | source
+end
 
-function proxy 
-    set -x -g https_proxy http://127.0.0.1:6152;
-    set -x -g http_proxy http://127.0.0.1:6152;
+function proxy_setting
+    set -x -g https_proxy http://127.0.0.1:6152
+    set -x -g http_proxy http://127.0.0.1:6152
     set -x -g all_proxy socks5://127.0.0.1:6153
 end
-proxy
 
-
-# Flutter
-set -gx PATH $HOME/.flutter/bin $PATH
-set -gx PATH $HOME/.config/bin $PATH
-# Language
-set -x LANG en_US.UTF-8
-# Editor
-set -gx EDITOR nvim
-
-# Git Configuration
-set GLOBAL_GIT_CONFIG_PATH $HOME/.gitconfig
-set GIT_CONFIG_PATH $HOME/.config/git/gitconfig 
-if test ! -e $GLOBAL_GIT_CONFIG_PATH
-    echo "not exist"
-    ln -s $HOME/.config/git/gitconfig $HOME/.gitconfig
+function fish_setup
+    set -U fish_greeting "--!"
+    # Language
+    set -x LANG en_US.UTF-8
+    # Editor
+    set -gx EDITOR nvim
+    fish_config theme choose "Rosé Pine"
 end
 
 
-# Aliases
-alias vi "nvim"
-alias vim "nvim"
-alias g git
-
-  #exa replace ls
-if type -q exa
-  alias ll "exa -l -g --icons"
-  alias lla "ll -a"
+function add_path
+    fish_add_path $HOME/.flutter/bin
+    fish_add_path $HOME/.config/bin
 end
 
-  # Chinese NPM Source
-alias cnpm "npm --registry=https://registry.npm.taobao.org \
---cache=$HOME/.npm/.cache/cnpm \
---disturl=https://npm.taobao.org/dist \
---userconfig=$HOME/.cnpmrc"
+function git_config
+    set GLOBAL_GIT_CONFIG_PATH $HOME/.gitconfig
+    set GIT_CONFIG_PATH $HOME/.config/git/gitconfig 
+    if test ! -e $GLOBAL_GIT_CONFIG_PATH
+        echo "not exist"
+        ln -s $HOME/.config/git/gitconfig $HOME/.gitconfig
+    end
+end
 
-# Key Bindings
- # peco
-bind \cr peco_select_history # Bind for peco select history to Ctrl+R
+function setup_aliases
+    alias vi "nvim"
+    alias vim "nvim"
+    alias g git
 
- # lfcd
-bind \co lfcd # Bind for lfcd to Ctrl+O
+    #use exa to replace ll, la
+    if type -q exa
+        alias ll "exa -l -g --icons"
+        alias lla "ll -a"
+    end
+    # Chinese NPM Source
+    alias cnpm "npm --registry=https://registry.npm.taobao.org \
+    --cache=$HOME/.npm/.cache/cnpm \
+    --disturl=https://npm.taobao.org/dist \
+    --userconfig=$HOME/.cnpmrc"
+end
+
+
+function key_binding
+    bind \cr peco_select_history
+    bind \co lfcd 
+end
+
+init_tools
+proxy_setting
+fish_setup
+add_path
+git_config
+setup_aliases
+key_binding
