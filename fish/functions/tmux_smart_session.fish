9ec86f49925f50a12d15df625e82edda2332d294
mx_in <mouxin1993@gmail.com>
2023-01-08 02:54:27 +0800

    feat(smart_session): query with peco

--- fish/functions/tmux_smart_session.fish
+++ fish/functions/tmux_smart_session.fish
@@ -1,69 +1,67 @@
 function tmux_smart_session
     #if not currently in tmux
+    set prompt "Switch to Session: "
     if test -z $TMUX
-        tmux is not active
-        echo "is not tmux"
+        echo tmux is not active
+        #save peco to variables
+        set z_select_result (zoxide query -l | peco --prompt $prompt)
+        echo z_select_result: $z_select_result
 
-        #save fzf to variables
-        set ZOXIDE_RESULT (zoxide query -l | fzf --reverse)
-        echo $ZOXIDE_RESULT
-
-        exit 1
         #if empty exit
-        if test -z $ZOXIDE_RESULT
-            echo "exit 0"
+        if test -z $z_select_result
+            exit 0
         end
 
-        set FOLDER (basename "$ZOXIDE_RESULT")
+        set folder (basename $z_select_result)
         #lookup tmux session name
-        set SESSION (tmux list-sessions | grep "$FOLDER" | awk '{print $1}')
-        set SESSION (string replace "$SESSION" : '')
-        echo "$SESSION"
+        set session (tmux list-sessions | grep $folder | awk '{print $1}')
+        set session (string replace : '' $session)
 
-        if test -z "$SESSION"
+        if test -z $session
             # session does not exist
             echo "session does not exist"
             # jump to directory
-            cd "$ZOXIDE_RESULT"
+            cd $z_select_result
             # create session
-            tmux new-session -s "$FOLDER"
+            tmux new-session -s $folder
         else
             # session exists
             echo "session exists"
             # attach to session
-            tmux attach -t "$SESSION"
+            tmux attach -t $session
         end
     else
         #tmux is active
         echo "is tmux"
-        #save fzf to variables
-        set ZOXIDE_RESULT (zoxide query -l | fzf --reverse)
+        #save peco to variables
+        set z_select_result (zoxide query -l | peco --prompt $prompt)
         #if no result exit
-        if test -z "$ZOXIDE_RESULT"
-            echo "exit 0"
+        echo z_select_result: $z_select_result
+        if test -z $z_select_result
+            exit 0
         end
 
-        set FOLDER (basename "$ZOXIDE_RESULT")
+        set folder (basename $z_select_result)
 
         #lookup tmux session name
-        set SESSION (tmux list-sessions | grep "$FOLDER" | awk '{print $1}')
-        set SESSION (string replace "$SESSION" : '')
+        set session (tmux list-sessions | grep $folder | awk '{print $1}')
+        set session (string replace : '' $session)
 
-        if test -z "$SESSION"
+        if test -z $session
             # session does not exist
             echo "session does not exist"
             # jump to directory
-            cd "$ZOXIDE_RESULT"
+            cd $z_select_result
             # create session
-            tmux new-session -d -s "$FOLDER"
+            tmux new-session -d -s $folder
             # attach to session
-            tmux switch-client -t "$FOLDER"
+            tmux switch-client -t $folder
         else
             # session exists
             echo "session exists"
             # attach to session
             # switch to tmux session
-            tmux switch-client -t "$SESSION"
+            tmux switch-client -t $session
         end
     end
 end
