7a8cacd5aeefd750835ad4da74ee87305279279f
mx_in <mouxin1993@gmail.com>
2023-01-11 16:19:43 +0800

    reafacotr(tmux_smart_session): duplicate code extract to functions

--- fish/functions/tmux_smart_session.fish
+++ fish/functions/tmux_smart_session.fish
@@ -1,73 +1,75 @@
 set prompt "switch to session:"
 
 function get_z_result
-        set select_result (zoxide query -l | peco --prompt $prompt)
-        #if empty exit
-        if test -z $select_result
-            exit 0
-        end
-        echo $select_result
+    set select_result (zoxide query -l | peco --prompt $prompt)
+    #if empty exit
+    if test -z $select_result
+        exit 0
+    end
+    echo $select_result
+end
+
+function try_to_get_in_session_list
+    if test -z $argv
+        return
+    end
+    set session_name $argv
+    echo (tmux list-sessions | grep $session_name | awk '{print $1}')
+end
+
+function display
+    if test -z $argv
+        return
+    end
+    if test -z $TMUX
+        echo $argv
+    else
+        tmux display $argv
+    end
+
 end
 
 function t_not_active
-        echo tmux is not active
-        #save peco to variables
-        set z_select_result $(get_z_result)
-        
-        set folder (basename $z_select_result)
-        #lookup tmux session name
-        set session (tmux list-sessions | grep $folder | awk '{print $1}')
-        echo $session
-        set session (string replace : '' $session)
-
-        breakpoint
-
-        if test -z $session
-            # session does not exist
-            echo "session does not exist"
-            # jump to directory
-            cd $z_select_result
-            # create session
-            tmux new-session -s $folder
-        else
-            # session exists
-            echo "session exists"
-            # attach to session
-            tmux attach -t $session
-        end
+    echo tmux is not active
+    #save peco to variables
+    set z_select_result $(get_z_result)
+    set session_name (basename $z_select_result)
+    #look for session
+    if test -z $(try_to_get_in_session_list $session_name)
+        # session does not exist
+        display "creating session: $session_name"
+        # jump to directory
+        cd $z_select_result
+        # create session
+        tmux new-session -s $session_name
+    else
+        # session exists
+        display "attach to exist session: $session_name"
+        # attach to session
+        tmux attach -t $session_name
+    end
 end
 
 function t_active
-        echo "tmux is active"
         set z_select_result $(get_z_result)
+        set session_name (basename $z_select_result)
 
-        set folder (basename $z_select_result)
-
-        #lookup tmux session name
-        set session (tmux list-sessions | grep $folder | awk '{print $1}')
-        set session (string replace : '' $session)
-
-        if test -z $session
-            # session does not exist
-            echo "session does not exist"
-            # jump to directory
+        if test -z (try_to_get_in_session_list $session_name)
+            display "Creating and swith to session: $session_name"
             cd $z_select_result
-            # create session
-            tmux new-session -d -s $folder
+            # create session -d for default size -s for name
+            tmux new-session -d -s $session_name
             # attach to session
-            tmux switch-client -t $folder
+            tmux switch-client -t $session_name
         else
-            # session exists
-            echo "session exists"
-            # attach to session
+            display "Switch to exists session: $session_name"
             # switch to tmux session
-            tmux switch-client -t $session
+            tmux switch-client -t $session_name
         end
 end
 
 function tmux_smart_session
-    #if not currently in tmux
-    set prompt "Switch to Session: "
+
     if test -z $TMUX
         t_not_active
     else
