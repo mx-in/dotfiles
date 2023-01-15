8b87873f54af26464b48a37548972a1500b14cc0
mx_in <mouxin1993@gmail.com>
2023-01-15 23:19:40 +0800

    feat(vim): cursor

--- nvim/lua/mx-in/set.lua
+++ nvim/lua/mx-in/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
