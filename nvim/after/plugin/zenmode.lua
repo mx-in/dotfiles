acc55733cea90a4d8817f476008575c9b92c6983
mx_in <mouxin1993@gmail.com>
2023-04-30 19:55:14 +0800

    feat(zen-model): update setting

--- nvim/after/plugin/zenmode.lua
+++ nvim/after/plugin/zenmode.lua
@@ -1,48 +1,27 @@
 local status, zen = pcall(require, "zen-mode");
 if (not status) then return end
 
-zen.setup({
-    {
-        window = {
-            width = .85, -- width of the Zen window
-            height = 1, -- height of the Zen window
-            options = {
-                -- signcolumn = "no", -- disable signcolumn
-                -- number = false, -- disable number column
-                -- relativenumber = false, -- disable relative numbers
-                -- cursorline = false, -- disable cursorline
-                -- cursorcolumn = false, -- disable cursor column
-                -- foldcolumn = "0", -- disable fold column
-                -- list = false, -- disable whitespace characters
-            },
-        },
-        plugins = {
-            -- disable some global vim options (vim.o...)
-            -- comment the lines to not apply the options
-            options = {
-                enabled = true,
-                ruler = false, -- disables the ruler text in the cmd line area
-                showcmd = false, -- disables the command in the last line of the screen
-            },
-            twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
-            gitsigns = { enabled = false }, -- disables git signs
-            tmux = { enabled = false }, -- disables the tmux statusline
-            -- this will change the font size on kitty when in zen mode
-            -- to make this work, you need to set the following kitty options:
-            -- - allow_remote_control socket-only
-            -- - listen_on unix:/tmp/kitty
-            kitty = {
-                enabled = false,
-                font = "+4", -- font size increment
-            },
-        },
-        -- callback where you can add custom code when the Zen window opens
-        on_open = function(win)
-        end,
-        -- callback where you can add custom code when the Zen window closes
-        on_close = function()
-        end,
-    }
-})
+zen.setup {
+  window = {
+    backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+    width = 0.85,    -- width of the Zen window
+    height = 1,      -- height of the Zen window
+  },
+  plugins = {
+    options = {
+      enabled = true,
+      ruler = false,               -- disables the ruler text in the cmd line area
+      showcmd = false,             -- disables the command in the last line of the screen
+    },
+    twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+    gitsigns = { enabled = true }, -- disables git signs
+    tmux = { enabled = true },     -- disables the tmux statusline
+    alacritty = {
+      enabled = false,
+      font = "+2", -- font size
+    },
+  },
+}
+
 
 vim.keymap.set("n", "<leader>zen", vim.cmd.ZenMode)
