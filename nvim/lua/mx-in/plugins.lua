cf8cff344ec217e64fb8201b5f755d956b55b01f
mx_in <mouxin1993@gmail.com>
2023-05-05 15:56:58 +0800

    feat:(nvim): surround

--- nvim/lua/mx-in/plugins.lua
+++ nvim/lua/mx-in/plugins.lua
@@ -212,5 +212,15 @@ local plugins = {
     'wakatime/vim-wakatime'
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
 }
 require('lazy').setup(plugins, {})
