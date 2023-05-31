9f82dc0f02245ddc45272821fa6020804ebb093a
mx_in <mouxin1993@gmail.com>
2023-05-31 15:28:06 +0800

    fix(fish): update index immediately

--- fish/functions/get_openai_key.fish
+++ fish/functions/get_openai_key.fish
@@ -5,14 +5,21 @@ function get_openai_key -a refetch
 
     if test "$refetch" = "-r"
       curl 'https://freeopenai.xyz/api.txt' --compressed > $keys_file
-      echo 'index:1' >> $keys_file
+      echo 'index:0' >> $keys_file
     end
 
     set start_line (grep 'index' $keys_file | awk -F: '{print $2}'| string replace -a '\s' '')
     set line_count (wc -l $keys_file | awk '{print $1}' | string replace -a '\s' '')
+    set start_line (math $start_line + 1)
     set line_count (math $line_count - 1)
 
     echo "$line_count keys total, start to test from no.$start_line"
+
+    if test $start_line -gt $line_count
+      echo "no more keys"
+      return
+    end
+
     for i in (seq (math $start_line) $line_count)
       set key (sed -n "$i"p $keys_file | string replace '' '')
       set test_result (echo (test_openai_key $key))
@@ -20,6 +27,7 @@ function get_openai_key -a refetch
 
       if test "$test_result" = "true"
         echo "😎: $key"
+        echo $key | pbcopy && say "find a valid key"
         #update index
         sed -i bak "s/index:$start_line/index:$i/" $keys_file
 
