8ebad6e3a512ed9bfbbc2435108d27afbee7fd66
mx_in <mouxin1993@gmail.com>
2023-06-27 13:51:05 +0800

    feat: copilot cli

---
+++ fish/functions/copilot_gh-assist.fish
@@ -0,0 +1,14 @@
+function copilot_gh-assist
+    set TMPFILE (mktemp)
+    trap 'rm -f $TMPFILE' EXIT
+    if /opt/homebrew/bin/github-copilot-cli gh-assist $argv --shellout $TMPFILE
+        if test -e $TMPFILE
+            set FIXED_CMD (cat $TMPFILE)
+            eval $FIXED_CMD
+        else
+            echo "Apologies! Extracting command failed"
+        end
+    else
+        return 1
+    end
+end
