aa1025139487933ff31fc6371edeaf99c1810d90
mx_in <mouxin1993@gmail.com>
2023-06-27 13:31:44 +0800

    chore: fix keymap conflict

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -104,7 +104,7 @@ lsp.on_attach(function(client, bufnr)
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('K', vim.lsp.buf.hover, "Hover Document")
-  nmap('<C-h>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  nmap('H', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
