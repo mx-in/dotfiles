483620885a6d41e8520874e854d46829e477c702
mx_in <mouxin1993@gmail.com>
2023-12-16 16:43:52 +0800

    chore: key map and color

--- nvim/lua/mx-in/remap.lua
+++ nvim/lua/mx-in/remap.lua
@@ -51,7 +51,7 @@ keymap.set('n', '<C-d>', '<C-d>zz')
 keymap.set('n', '<C-u>', '<C-u>zz')
 
 -- paste without overwriting the default register
---keymap.set('x', '<leader>p', [['_dP]])
+keymap.set('x', '<leader>p', [['_dP]])
 
 keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
