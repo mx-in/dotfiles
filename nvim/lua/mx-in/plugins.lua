999f47ea79c0b091887b491896371ba1132db920
mx_in <mouxin1993@gmail.com>
2025-08-27 23:42:28 +0800

    chore(nvim): rm venv selector

--- nvim/lua/mx-in/plugins.lua
+++ nvim/lua/mx-in/plugins.lua
@@ -230,22 +230,6 @@ local plugins = {
     end,
     lazy = true,
   },
-  {
-    'linux-cultist/venv-selector.nvim',
-    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-    opts = {
-      -- Your options go here
-      -- name = "venv",
-      -- auto_refresh = false
-    },
-    event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-    keys = {
-      -- Keymap to open VenvSelector to pick a venv.
-      { '<leader>vs', '<cmd>VenvSelect<cr>' },
-      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-    },
-  },
 
   {
     "ray-x/lsp_signature.nvim",
